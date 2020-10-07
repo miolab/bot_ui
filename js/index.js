436c14d1075a989716a1a238a7c11a884065af8b
im <poson_hemp@yahoo.co.jp>
2020-10-07 20:03:51 +0900

    add form and response

--- js/index.js
+++ js/index.js
@@ -4,11 +4,24 @@ const botUi = new BotUI("my-botui-app");
 
 botUi.message
   .add({
-    content: "Hello, im.",
+    content: "Hello, this is 'IM Bot' !",
   })
   .then(() => {
     botUi.message.add({
       content: "はじめまして！",
-      delay: 500,
+      delay: 800,
+    });
+  })
+  .then(() => {
+    botUi.message.add({
+      content: "あなたのことは、どのようにお呼びすれば良いでしょう？",
+      delay: 1600,
+      cssClass: "js-add-space",
+    });
+  })
+  .then(() => {
+    return botUi.action.text({
+      action: { placeholder: "名前を入力してください..." },
+      delay: 2000,
     });
   });
