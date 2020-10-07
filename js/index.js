924ea32b1f2f22365dda216eed192e59b506d00c
im <poson_hemp@yahoo.co.jp>
2020-10-08 08:54:19 +0900

    add delay time of init appear msg

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const botUi = new BotUI("my-botui-app");
 botUi.message
   .add({
     content: "Hello, this is 'IM Bot' !",
+    delay: 150,
   })
   .then(() => {
     botUi.message.add({
@@ -21,7 +22,9 @@ botUi.message
   })
   .then(() => {
     return botUi.action.text({
-      action: { placeholder: "名前を入力してください..." },
+      action: {
+        placeholder: "名前を入力してください..."
+      },
       delay: 2000,
     });
   });
